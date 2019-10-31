75274fed47f5c4149d4cb15edfe2970e4f7cd4a1
Ninte <ninte.dev@gmail.com>
2019-10-31 13:23:01 +0000

    working app

--- imports/api/links/methods.js
+++ imports/api/links/methods.js
@@ -1,20 +1,20 @@
 // Methods related to links
 
-import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor'
+import { check } from 'meteor/check'
 // import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Eits } from './links.js';
+import { Eits } from './links.js'
 
 Meteor.methods({
   'eits.insert'(firstname, surname, country, age) {
-    if (!Meteor.user()) {
-      return new Meteor.Error('Permission Denied');
+    if (!this.userId) {
+      throw new Meteor.Error('Permission Denied')
     }
 
-    check(firstname, String);
-    check(surname, String);
-    check(country, String);
-    check(age, String);
+    check(firstname, String)
+    check(surname, String)
+    check(country, String)
+    check(age, String)
 
     return Eits.insert({
       firstname,
@@ -22,36 +22,36 @@ Meteor.methods({
       country,
       age,
       createdAt: new Date(),
-      editor: Meteor.userId(),
-    });
+      editor: this.userId,
+    })
   },
 
   'eits.remove'(_id, editor) {
-    if (!Meteor.userId()) {
-      throw new Meteor.Error('Permission denied: can not delete.');
+    if (!this.userId) {
+      throw new Meteor.Error('Permission denied: can not delete.')
     }
 
-    if (Meteor.userId() !== editor) {
-      throw new Meteor.Error('Permission denied: can not delete.');
+    if (this.userId !== editor) {
+      throw new Meteor.Error('Permission denied: can not delete.')
     }
 
     return Eits.remove({
       _id,
-    });
+    })
   },
 
   'eits.setChecked'(_id, checked) {
     return Eits.update(_id, {
       $set: { checked: checked },
-    });
+    })
   },
 
   'eits.bulkDelete'() {
-    const checkedEits = Eits.find({ checked: true }).fetch();
+    const checkedEits = Eits.find({ checked: true }).fetch()
 
     checkedEits.forEach(function(selected) {
-      Eits.remove(selected._id);
-    });
+      Eits.remove(selected._id)
+    })
   },
 
   // 'eits.edit'(_id) {
@@ -69,6 +69,6 @@ Meteor.methods({
         country: country,
         age: age,
       },
-    });
+    })
   },
-});
+})
